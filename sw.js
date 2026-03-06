1bd1caf7a9415468c65fd15234470969e64d12df
goloka-gid <fedorovtcev@gmail.com>
2026-03-06 09:21:23 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = 'elli-world-v1';
+const STATIC_ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './config.js',
+    './data.js',
+    './users.json',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('[SW] Кэширование статики');
+                return cache.addAll(STATIC_ASSETS);
+            })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    if (!event.request.url.startsWith('http')) return;
+
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request).then(networkResponse => {
+                if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+                    return networkResponse;
+                }
+                const responseToCache = networkResponse.clone();
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseToCache);
+                });
+                return networkResponse;
+            }).catch(() => {});
+        })
+    );
+});
